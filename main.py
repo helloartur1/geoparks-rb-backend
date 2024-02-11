b9bc5db1cf3d702e5f0eff0b93f61eb1ec8c9d80
Kopurkul <kopurkul@mail.ru>
2024-02-11 15:40:03 +0500

    убрал конфликт с изображениями

--- main.py
+++ main.py
@@ -2,7 +2,26 @@ from fastapi import FastAPI
 from services import admin_service, auth_service, geoobjects_service, geoparks_service, photo_service
 
 
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
+
+
+app = FastAPI()
+app.mount("/geopark_image", StaticFiles(directory="geopark_image"), name="geopark_image")
+origins = [
+    "http://localhost",
+    "http://localhost:4200",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(admin_service.router)
